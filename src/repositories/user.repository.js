ee686e1e315f094f76d700bfdb2bd057b5542831
qndaa98 <qndaa98@gmail.com>
2025-04-01 15:54:06 +0200

    delete unecessary files

--- src/repositories/user.repository.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRepository = void 0;
-const database_1 = require("../config/database");
-const User_1 = require("../entities/User");
-exports.UserRepository = database_1.AppDataSource.getRepository(User_1.User);
-//# sourceMappingURL=user.repository.js.map
